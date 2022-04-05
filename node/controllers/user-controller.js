f41bbdbdf3ccba9d28b66e8499be998882164624
Dimitar Mihaylov <dimitar.mihaylove@gmail.com>
2022-04-05 23:31:28 +0300

    Remove endpoint; Use mongoose filter instead of array.filter

--- node/controllers/user-controller.js
+++ node/controllers/user-controller.js
@@ -9,51 +9,18 @@ const entityNotDeleted = require("../middlewares/entity-not-deleted-middleware")
 const entityExists = require("../middlewares/entity-exists-middleware");
 
 router.post("/", authenticated, async (req, res) => {
-  const allUsers = await User.find();
   const roleFilter = req.body.role;
+  const users = roleFilter ? await User.find({ role: roleFilter }) : await User.find();
 
-  const filteredUsers = allUsers.filter(user => !roleFilter || user.role === roleFilter);
-  if (!allUsers) return sendErrorResponse(req, res, 204, `No users`);
-  return res.status(200).send(filteredUsers);
-});
-router.post("/", authenticated, async (req, res) => {
-  const { error } = await userValidation(req.body);
-  if (error) return sendErrorResponse(req, res, 400, error.details[0].message, error);
-
-  const user = await User.findOne({ $or: [{ username: req.body.username }, { email: req.body.email }],});
-  if (user) return sendErrorResponse(req, res, 400, `User already exists`);
-
-  const newUser = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    username: req.body.username,
-    password: req.body.password,
-    projects: req.body.projects,
-    tasks: req.body.tasks,
-    role: req.body.role ? req.body.role : "user",
-    photo: req.body.photo ?? null
-  });
-
-  try {
-    const savedUser = await newUser.save();
-    delete(savedUser.password)
-        const uri = req.baseUrl + `/${savedUser.id}`;
-        console.log('Created User: ', savedUser.id);
-        return res.location(uri).status(201).json(savedUser);
-  } catch (error) {
-    return (req, res, 500, `Server error: ${error}`, error);
-  }
+  if (!users) return sendErrorResponse(req, res, 204, `No users`);
+  return res.status(200).send(users);
 });
 
-//{userId}
 router.get( "/:userId",
   authenticated,
   async (req, res) => {
     const { userId } = req.params;
     if (!userId) return sendErrorResponse(req, res, 400, `Missing userId`);
-
-    const schema = User;
 
     const user = await User.findOne({ _id: userId });
     if (!user) return sendErrorResponse(req, res, 400, `There is no user with this id`);
