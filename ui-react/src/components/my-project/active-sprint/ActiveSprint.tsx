69af1597ffa32243819c5efb7021c59f5c831e30
Dimitar Mihaylov <dimitar.mihaylove@gmail.com>
2022-06-29 23:54:34 +0300

    Minor fixes

--- ui-react/src/components/my-project/active-sprint/ActiveSprint.tsx
+++ ui-react/src/components/my-project/active-sprint/ActiveSprint.tsx
@@ -10,8 +10,9 @@ import {SprintApi} from "../../../api/sprint.api";
 import styles from './ActiveSprint.module.scss'
 import {IconButton, Menu, MenuItem} from "@mui/material";
 import {MoreHoriz} from "@mui/icons-material";
-import {IIssue, ISprint} from "../../../models";
+import {IIssue, ISprint, IUser} from "../../../models";
 import {IssueApi} from "../../../api/issue.api";
+import {UserApi} from "../../../api/user.api";
 
 const ActiveSprint = () => {
     const issues = useIssues();
@@ -20,6 +21,7 @@ const ActiveSprint = () => {
     const params = useParams();
     const project = useProject();
     const firstLoad = useRef(true);
+    const [developers, setDevelopers] = useState<IUser[]>([]);
     const [activeSprint, setActiveSprint] = useState<ISprint | null>(null);
 
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -35,6 +37,7 @@ const ActiveSprint = () => {
         if (firstLoad?.current && location.pathname.includes('projects') && params.id !== project?.id) {
             dispatch(getProjectAsyncAction(params.id));
             firstLoad.current = false;
+            UserApi.getUsers({role: 'developer'}).then(developers => setDevelopers(developers));
         }
     }, [location, params, dispatch, project])
 
@@ -60,6 +63,16 @@ const ActiveSprint = () => {
         setAnchorEl(null);
     }, [project, activeSprint, dispatch])
 
+    const getDeveloperName = useCallback((userId: string) => {
+        const developer = developers.find(x => x.id === userId);
+
+        if (developer) {
+            return `${developer.firstName} ${developer.lastName}`
+        }
+
+        return '';
+    }, [developers])
+
     return <div>
         {activeSprint ? <>
             <h1 className={styles.title}>Active sprint</h1>
@@ -69,7 +82,8 @@ const ActiveSprint = () => {
                         <h4>{Status[x]}</h4>
                         <ul className={styles.list}>
                             {issues.filter(i => i.status === x).map(i => <li className={styles.listItem} key={i.id}>
-                                {i.title}
+                                <span className={styles.issueTitle}>{i.title}</span>
+                                <span className={styles.developer}>{getDeveloperName(i.assignedTo)}</span>
                                 <IconButton onClick={handleClick}>
                                     <MoreHoriz/>
                                 </IconButton>
